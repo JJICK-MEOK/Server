6999b4cd78f350f3658b9c3be493e2cfbf0e0717
JOO <107450745+jucheonsu@users.noreply.github.com>
2026-05-09 13:11:42 +0900

    [Feat]: 메시지와 제약조건 불일치의 해결

--- src/main/java/com/jjikmeok/app/domain/auth/dto/request/SignupReq.java
+++ src/main/java/com/jjikmeok/app/domain/auth/dto/request/SignupReq.java
@@ -20,7 +20,7 @@ public class SignupReq {
     @NotBlank
     @Size(min = 8, max = 64)
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*]).+$",
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+=\\-\\[\\]{}|;:,.<>?/~`])[a-zA-Z\\d!@#$%^&*()_+=\\-\\[\\]{}|;:,.<>?/~`]+$",
             message = "비밀번호는 영문, 숫자, 특수문자를 각각 1자 이상 포함해야 합니다."
     )
     private String password;

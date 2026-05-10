086a64187ea69c67322b9ac7d031b317e1e122c2
Juwoojae <juwqq1234@naver.com>
2026-05-10 19:56:56 +0900

    [Feat]: jwt 핵심 컴포넌트 분리 및 토큰 유틸리티 구현 - Jwt 토큰 claim  설계 - access, refresh 토큰 생성및 처리, 검증로직 구현 - Jwt 커스텀 예외 추가 - Jwt 토큰에서 발생할수있는 errorCode 추가 - propertiesScan 추가

--- src/main/java/com/jjikmeok/app/global/common/exception/ErrorCode.java
+++ src/main/java/com/jjikmeok/app/global/common/exception/ErrorCode.java
@@ -18,6 +18,7 @@ public enum ErrorCode {
     // Auth
     AUTH_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_401", "인증이 필요합니다."),
     AUTH_INVALID_CODE(HttpStatus.UNAUTHORIZED, "AUTH_401_CODE", "유효하지 않은 소셜 인가 코드입니다."),
+    AUTH_INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_401_CREDENTIALS", "이메일 또는 비밀번호가 올바르지 않습니다."),
     AUTH_ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_401_ACCESS", "Access Token이 만료되었습니다."),
     AUTH_REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_401_REFRESH", "Refresh Token이 만료되었습니다. 다시 로그인이 필요합니다."),
     AUTH_FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_403", "해당 API에 대한 접근 권한(관리자 등)이 없습니다."),
@@ -25,6 +26,17 @@ public enum ErrorCode {
     MAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_500_MAIL", "인증 메일 발송에 실패했습니다."),
     MAIL_VERIFICATION_CODE_INVALID(HttpStatus.BAD_REQUEST, "AUTH_400_MAIL_CODE", "인증번호가 올바르지 않습니다."),
     MAIL_VERIFICATION_CODE_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_401_MAIL_EXPIRED", "인증번호가 만료되었습니다."),
+
+    // JWT
+    JWT_INVALID_SECRET(HttpStatus.INTERNAL_SERVER_ERROR, "JWT_500_SECRET", "JWT 서명 키 설정이 올바르지 않습니다."),
+    JWT_EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "JWT_401_EMPTY", "JWT 토큰이 비어 있습니다."),
+    JWT_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT_401_EXPIRED", "JWT 토큰이 만료되었습니다."),
+    JWT_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "JWT_401_INVALID", "유효하지 않은 JWT 토큰입니다."),
+    JWT_INVALID_SIGNATURE(HttpStatus.UNAUTHORIZED, "JWT_401_SIGNATURE", "JWT 서명이 유효하지 않습니다."),
+    JWT_MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT_401_MALFORMED", "형식이 올바르지 않은 JWT 토큰입니다."),
+    JWT_UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT_401_UNSUPPORTED", "지원하지 않는 JWT 토큰입니다."),
+    JWT_INVALID_SUBJECT(HttpStatus.UNAUTHORIZED, "JWT_401_SUBJECT", "JWT subject 값이 올바르지 않습니다."),
+    JWT_MISSING_ROLE_CLAIM(HttpStatus.UNAUTHORIZED, "JWT_401_ROLE", "JWT role claim이 존재하지 않습니다."),
     // OAuth
     INVALID_PROVIDER(HttpStatus.BAD_REQUEST, "AUTH_400_PROVIDER", "지원하지 않는 소셜 로그인 provider입니다.");
 

773db0d3822e58cd1d91358e1dd84657fb8b2e42
JOO <107450745+jucheonsu@users.noreply.github.com>
2026-05-18 21:26:46 +0900

    [Refactor]: 불필요한 java docs 제거

--- src/main/java/com/jjikmeok/app/domain/tag/repository/TagRepository.java
+++ src/main/java/com/jjikmeok/app/domain/tag/repository/TagRepository.java
@@ -7,15 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * Tag repository.
- * <p>
- * 태그 조회와 온보딩 검증에 필요한 접근 메서드를 제공한다.
- *
- * @author Codex
- * @version 1.0
- * @since 2026-05-16
- */
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
     List<Tag> findAllByIdIn(Collection<Long> ids);

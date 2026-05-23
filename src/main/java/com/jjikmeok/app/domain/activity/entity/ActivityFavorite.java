9a9f2aa6c00c8b4ff99c8ba44b8a2ac8a6299ba9
JOO <107450745+jucheonsu@users.noreply.github.com>
2026-05-23 14:03:46 +0900

    #28 [Feat] 활동 찜 기능 구현 (#29)

---
+++ src/main/java/com/jjikmeok/app/domain/activity/entity/ActivityFavorite.java
@@ -0,0 +1,37 @@
+package com.jjikmeok.app.domain.activity.entity;
+
+import com.jjikmeok.app.domain.user.entity.User;
+import com.jjikmeok.app.global.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "activity_favorites", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_activity_favorites_user_activity", columnNames = {"user_id", "activity_id"})
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ActivityFavorite extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false)
+    private Activity activity;
+
+    public static ActivityFavorite create(User user, Activity activity) {
+        ActivityFavorite favorite = new ActivityFavorite();
+        favorite.user = user;
+        favorite.activity = activity;
+        return favorite;
+    }
+}

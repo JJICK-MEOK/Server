f378ac78a6d9a6808dd81bf1e5f1fb4833917b05
JOO <107450745+jucheonsu@users.noreply.github.com>
2026-05-29 18:58:31 +0900

    #60 [Remove] activity 하위 이관 완료 구 경로 삭제 (#61)
    [Remove]
    - activity 하위 이관 완료 구 경로 삭제

--- src/main/java/com/jjikmeok/app/domain/activity/dto/request/ActivityFavoriteRequest.java
+++
@@ -1,9 +0,0 @@
-package com.jjikmeok.app.domain.activity.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ActivityFavoriteRequest(
-        @NotNull(message = "활동 ID는 필수입니다.")
-        Long activityId
-) {
-}

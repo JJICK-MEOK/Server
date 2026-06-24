e176055addb6c368514443c65decb613ffc6adc0
user040131 <seungjub270@gmail.com>
2026-06-24 20:10:54 +0900

    swagger 관련 수정

--- src/main/java/com/jjikmeok/app/domain/activity/dto/response/page/ActivityImageItemResponse.java
+++
@@ -1,9 +0,0 @@
-package com.jjikmeok.app.domain.activity.dto.response.page;
-
-public record ActivityImageItemResponse(
-        Long id,
-        String imageUrl,
-        Integer sortOrder,
-        Boolean thumbnail
-) {
-}
